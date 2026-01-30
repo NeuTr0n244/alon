7afe0e2bd22a3d13358cba93f1d96ece95096c32
NeuTr0n244 <vito01hugo02@gmail.com>
2026-01-30 03:46:43 -0300

    Fix: Remove unused onCharacterLoad prop

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,9 @@ import { MainLayout } from '@/components/layout/MainLayout';
 import { NewTokensColumn } from '@/components/columns/NewTokensColumn';
 import { KnowledgeBase } from '@/components/KnowledgeBase';
 import { CharacterCanvas } from '@/components/character/CharacterCanvas';
-import { CharacterModel } from '@/types/character';
-import { LipSyncController } from '@/lib/three/lipSyncController';
 
 function CharacterColumn() {
-  const handleCharacterLoad = (model: CharacterModel, lipSyncController: LipSyncController) => {
-    console.log('[App] Character loaded');
-  };
-
-  return <CharacterCanvas onCharacterLoad={handleCharacterLoad} />;
+  return <CharacterCanvas />;
 }
 
 export default function Home() {
